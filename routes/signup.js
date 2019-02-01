20be1041569440e3d9e6efceab8e942b069c97c7
Luis <lugaliguori@gmail.com>
2019-02-01 12:20:45 -0400

    Eliminando credenciales de firebase

--- routes/signup.js
+++ routes/signup.js
@@ -13,41 +13,46 @@ api.post("/", function (req, res){
 
     db.ref().child("Users").orderByChild("email").equalTo(req.body.email).once("value", snapshot => {
         if (snapshot.exists()){
-            console.log("existe!");
-            res.json({ status:400 , message: "There is already an account associated with this email"});
-            return
+            console.log("Este email ya se encuentra asociado");
+            var email = snapshot.val();
         }
-    });
-
-    db.ref().child("Users").orderByChild("username").equalTo(req.body.usuario).once("value", snapshot => {
-        if (snapshot.exists()){
-            console.log("existe!");
-            res.json({ status:400 , message: "This username is not available"});
-            return
-        }
-    });
-
-    var postRef = db.ref().child("Users");
+        if (email == null){
+            db.ref().child("Users").orderByChild("username").equalTo(req.body.usuario).once("value", snapshot => {
+                if (snapshot.exists()){
+                    var username = snapshot.val();
+                }
+                if (username == null){
+                    var postRef = db.ref().child("Users");
     
-    var newPostRef = postRef.push().set({
-        name: req.body.nombre,
-        lastname: req.body.apellido,
-        email: req.body.email,
-        phone: req.body.telefono,
-        username: req.body.usuario,
-        password: req.body.password
-    }, function (error){
-        if (error){
-            res.send(error);
-        } else {
-            res.json({
-                status: "201",
-                message: "Saved succesfully"
+                    var newPostRef = postRef.push().set({
+                        name: req.body.nombre,
+                        lastname: req.body.apellido,
+                        email: req.body.email,
+                        phone: req.body.telefono,
+                        username: req.body.usuario,
+                        password: req.body.password
+                    }, function (error){
+                        if (error){
+                            res.send(error);
+                        } else {
+                            res.json({status: 201, message: "User created succesfully"});
+                        }
+                    });                
+                } else {
+                    res.json({status: 400, message: "The username is not available"});
+                }
+            }, function (error){
+                console.log("Could not retrieve data from database", + error.code);
+                res.status(500).send("Could not retrieve data " + error);
             });
-        }
+         } else {
+             res.json({status: 400, message: "This email is already associated with an account"});
+         }
+    
+    }, function (error){
+        console.log("Could not retrieve data from database", + error.code);
+        res.status(500).send("Could not retrieve data " + error);
     });
-
-
 
 });
 
