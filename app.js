da0d66e7b889430cc810bb63bbb3a9b0b55527d9
Luis <lugaliguori@gmail.com>
2019-03-19 17:13:24 -0400

    f

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ var config = {
   type: process.env.firebase_type,
   project_id: process.env.firebase_project_id,
   private_key_id: process.env.firebase_private_key_id,
-  private_key: process.env.firebase_private_key,
+  private_key: process.env.firebase_private_key.replace(/\\n/g,'\n'),
   client_email: process.env.firebase_client_email,
   client_id: process.env.firebase_client_id,
   auth_uri: process.env.firebase_auth_uri,
