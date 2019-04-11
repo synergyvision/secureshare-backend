bf19d33707cc9312bc17e77716074bcf561cade0
Luis <lugaliguori@gmail.com>
2019-04-11 16:14:14 -0400

    d

--- routes/comment.js
+++ routes/comment.js
@@ -9,6 +9,7 @@ api.use(bodyParser.urlencoded({ extended: false }));
 
 api.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
