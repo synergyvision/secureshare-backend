57d2f9d3a2fad59f63806d99faadf28117f95ebb
Luis <lugaliguori@gmail.com>
2019-07-17 16:55:23 -0400

    Didn't make sense not to live for fun

--- routes/login.js
+++ routes/login.js
@@ -18,8 +18,7 @@ api.use(function(req, res, next) {
   var decryptPassword = async (string) => {
     //passphrase = keys.server_passphrase;
     //privateKey = keys.server_private_key;
-    var privateKey = process.env.server_private_key.replace(/(?:\\[r])+/g, "")
-    privateKey = privateKey.replace(/(?:\\[n])+/g, "\n")
+    var privateKey = process.env.server_private_key.replace(/(?:\\[r])+/g, "\n")
     console.log(privateKey)
     var passphrase = process.env.server_passphrase
     console.log(await openpgp.key.readArmored(privateKey))
