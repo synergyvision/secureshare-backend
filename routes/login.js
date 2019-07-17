1e9116203f13768ab1948a0cca15cad1b1f3fbd1
Luis <lugaliguori@gmail.com>
2019-07-17 16:18:40 -0400

    once told me

--- routes/login.js
+++ routes/login.js
@@ -19,6 +19,7 @@ api.use(function(req, res, next) {
     //passphrase = keys.server_passphrase;
     //privateKey = keys.server_private_key;
     var privateKey = process.env.server_private_key.replace(/\\n/g,'\n')
+    console.log(privateKey)
     var passphrase = process.env.server_passphrase
     console.log(await openpgp.key.readArmored(privateKey))
     var privKeyObj = (await openpgp.key.readArmored(privateKey)).keys[0]
