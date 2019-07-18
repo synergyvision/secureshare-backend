62e8eb983a8a8508c033470cfc78ab958708cac2
Luis <lugaliguori@gmail.com>
2019-07-18 09:43:25 -0400

    changing variable names

--- routes/repos.js
+++ routes/repos.js
@@ -53,8 +53,8 @@ api.use(function(req, res, next) {
 var decryptPassword = async (string) => {
     //passphrase = keys.server_passphrase;
     //privateKey = keys.server_private_key;
-    var publicKey = process.env.server_private_key.replace(/\\n/g,'\n')
-    var publicKey = process.env.server_passphrase
+    var privateKey = process.env.server_private_key
+    var server_passphrase = process.env.server_passphrase
     var privKeyObj = (await openpgp.key.readArmored(privateKey)).keys[0]
 	await privKeyObj.decrypt(passphrase)
 	const options = {
