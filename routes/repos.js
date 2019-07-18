605b101cc8069ac43bd64bde918fbd14d0f391cc
Luis <lugaliguori@gmail.com>
2019-07-18 09:54:15 -0400

    console.log

--- routes/repos.js
+++ routes/repos.js
@@ -88,6 +88,7 @@ api.post('/:userid/getToken', function (req,res){
 
             user = req.body.username;
             password = req.body.password
+            console.log(password);
             password = decryptPassword(password);
             password.then(function (password){
                 var options = {
