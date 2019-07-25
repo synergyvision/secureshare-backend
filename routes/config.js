d48309a6aad184527634420241dfe9eca17ec936
Luis <lugaliguori@gmail.com>
2019-07-25 16:33:11 -0400

    adding dependencies

--- routes/config.js
+++ routes/config.js
@@ -1,7 +1,9 @@
 //var credentials = require('../keys.json');
 var express = require("express");
 var admin = require("firebase-admin");
+var bodyParser = require("body-parser");
 var api = express.Router();
+
 
 api.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -9,6 +11,8 @@ api.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, Authorization, X-Requested-With, Content-Type, Accept");
     next();
   });
+
+  api.use(bodyParser.urlencoded({ extended: false }));
 
   var publicKey = process.env.server_public_key.replace(/\\n/g,'\n');
   var facebookId = process.env.facebook_id_app;
@@ -28,7 +32,9 @@ api.get('/serverKeys', function (req,res){
 api.post('/facebookId', function (req,res){
     var encoded = req.headers.authorization.split(' ')[1]
     admin.auth().verifyIdToken(encoded).then(function(decodedToken) {
+        uid = req.body.uid
         if (decodedToken.uid == uid){
+            
             res.status(200).json({
                 status:200,
                 message: 'got app fb id',
